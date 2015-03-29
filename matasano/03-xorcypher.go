7dff8cf67e9d6c310fd747f71ead8b9b7a4ae9fe
Krishna Sundarram <krishna.sundarram@gmail.com>
2015-03-29 13:52:59 +0530

    Improve ranking algorithm

--- matasano/03-xorcypher.go
+++ matasano/03-xorcypher.go
@@ -5,31 +5,19 @@ const (
 	maxASCII = 126
 )
 
-var freqletters = []byte("ETAOINSHRDLU etaoinshrdlu")
-
+// approximate frequencies of letters used in the english language
+// ETAOIN = 3, SHRDLU = 2.5, FGPWY = 2, B = 1.5, KV = 1.0, JX = 0.5, QZ = 0.1
 // based on http://en.wikipedia.org/wiki/Letter_frequency
-var letterfreq2 = map[byte]float64{'a': 8.167, 'A': 8.167,
-	'b': 1.492, 'B': 1.492, 'c': 2.782, 'C': 2.782, 'd': 4.2583, 'D': 4.253,
-	'e': 12.702, 'E': 12.702, 'f': 2.228, 'F': 2.228, 'g': 2.015, 'G': 2.015,
-	'h': 6.094, 'H': 6.094, 'i': 6.966, 'I': 6.966, 'j': 0.153, 'J': 0.153,
-	'k': 0.772, 'K': 0.772, 'l': 4.025, 'L': 4.025, 'm': 2.406, 'M': 2.406,
-	'n': 6.749, 'N': 6.749, 'o': 7.507, 'O': 7.507, 'p': 1.929, 'P': 1.929,
-	'q': 0.095, 'Q': 0.095, 'r': 5.987, 'R': 5.987, 's': 6.327, 'S': 6.327,
-	't': 9.056, 'T': 9.056, 'u': 2.758, 'U': 2.758, 'v': 0.978, 'V': 0.978,
-	'w': 2.360, 'W': 2.360, 'x': 0.150, 'X': 0.150, 'y': 1.974, 'Y': 1.974,
-	'z': 0.074, 'Z': 0.074}
-
-// modified version of the map above, to give lesser weight to etaoin
-var letterfreq = map[byte]float64{'a': 3, 'A': 3,
+var letterfreq = map[byte]float64{'a': 3.0, 'A': 3.0,
 	'b': 1.5, 'B': 1.5, 'c': 2.5, 'C': 2.5, 'd': 2.5, 'D': 2.5,
-	'e': 3, 'E': 3, 'f': 2, 'F': 2, 'g': 2, 'G': 2,
-	'h': 2.5, 'H': 2.5, 'i': 3, 'I': 3, 'j': 0.5, 'J': 0.5,
-	'k': 1, 'K': 1, 'l': 2.5, 'L': 2.5, 'm': 2, 'M': 2,
-	'n': 3, 'N': 3, 'o': 3, 'O': 3, 'p': 2, 'P': 2,
+	'e': 3.0, 'E': 3.0, 'f': 2.0, 'F': 2.0, 'g': 2.0, 'G': 2.0,
+	'h': 2.5, 'H': 2.5, 'i': 3.0, 'I': 3.0, 'j': 0.5, 'J': 0.5,
+	'k': 1.0, 'K': 1.0, 'l': 2.5, 'L': 2.5, 'm': 2.0, 'M': 2.0,
+	'n': 3.0, 'N': 3.0, 'o': 3.0, 'O': 3.0, 'p': 2.0, 'P': 2.0,
 	'q': 0.1, 'Q': 0.1, 'r': 2.5, 'R': 2.5, 's': 2.5, 'S': 2.5,
-	't': 3, 'T': 3, 'u': 2.5, 'U': 2.5, 'v': 1, 'V': 1,
-	'w': 2, 'W': 2, 'x': 0.5, 'X': 0.5, 'y': 2, 'Y': 2,
-	'z': 0.1, 'Z': 0.1}
+	't': 3.0, 'T': 3.0, 'u': 2.5, 'U': 2.5, 'v': 1.0, 'V': 1.0,
+	'w': 2.0, 'W': 2.0, 'x': 0.5, 'X': 0.5, 'y': 2.0, 'Y': 2.0,
+	'z': 0.1, 'Z': 0.1, ' ': 3}
 
 // DecryptXor decrypts an array of bytes by XOR-ing it with all possible letters and choosing the
 // array with the highest rank.
@@ -55,13 +43,7 @@ func DecryptXor(input []byte) ([]byte, float64, byte) {
 func getrank(input []byte) float64 {
 	var rank float64
 	for _, l := range input {
-		// rank += letterfreq[l]
-
-		for j := range freqletters {
-			if l == freqletters[j] {
-				rank++
-			}
-		}
+		rank += letterfreq[l]
 	}
 	return rank
 }
