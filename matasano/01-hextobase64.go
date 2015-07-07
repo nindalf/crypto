da5d5bb9ab55ad8003cbefd3b59124318e0b1398
Krishna Sundarram <krishna.sundarram@gmail.com>
2015-07-08 01:49:57 +0530

    Add implementation for decoding base64

--- matasano/01-hextobase64.go
+++ matasano/01-hextobase64.go
@@ -2,7 +2,20 @@ package matasano
 
 import "bytes"
 
-var base64vals = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+var base64enc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+var base64dec = map[byte]byte{'a': 26, 'A': 0,
+	'b': 27, 'B': 1, 'c': 28, 'C': 2, 'd': 29, 'D': 3,
+	'e': 30, 'E': 4, 'f': 31, 'F': 5, 'g': 32, 'G': 6,
+	'h': 33, 'H': 7, 'i': 34, 'I': 8, 'j': 35, 'J': 9,
+	'k': 36, 'K': 10, 'l': 37, 'L': 11, 'm': 38, 'M': 12,
+	'n': 39, 'N': 13, 'o': 40, 'O': 14, 'p': 41, 'P': 15,
+	'q': 42, 'Q': 16, 'r': 43, 'R': 17, 's': 44, 'S': 18,
+	't': 45, 'T': 19, 'u': 46, 'U': 20, 'v': 47, 'V': 21,
+	'w': 48, 'W': 22, 'x': 49, 'X': 23, 'y': 50, 'Y': 24,
+	'z': 51, 'Z': 25, '0': 52, '1': 53, '2': 54, '3': 55,
+	'4': 56, '5': 57, '6': 58, '7': 59, '8': 60, '9': 61,
+	'+': 62, '/': 63, '=': 0}
+
 var hexvals = []byte("0123456789abcdef")
 
 // Hex2base64 converts a hex string to a base64 encoded string
@@ -78,7 +91,7 @@ func EncodeBase64(dst, src []byte) {
 		dst[j+2] = 64
 	}
 	for i := 0; i < len(dst); i++ {
-		dst[i] = base64vals[dst[i]]
+		dst[i] = base64enc[dst[i]]
 	}
 }
 
@@ -95,6 +108,42 @@ func pad(b []byte, n int) []byte {
 		b = append(b, l)
 	}
 	return b
+}
+
+// DecodeBase64 decodes a base64 encoded string
+// Skips all the error checking done in the stdlib implementation
+func DecodeBase64(dst, src []byte) int {
+	var written int
+	for len(src) > 0 {
+		var b0, b1, b2 byte
+
+		b0 = base64dec[src[0]] << 2
+
+		b0 |= base64dec[src[1]] >> 4
+		b1 = base64dec[src[1]] << 4
+
+		b1 |= base64dec[src[2]] >> 2
+		b2 = base64dec[src[2]] << 6
+
+		b2 |= base64dec[src[3]]
+
+		dst[0] = b0
+		dst[1] = b1
+		dst[2] = b2
+
+		src = src[4:]
+		dst = dst[3:]
+		written += 3
+		if len(src) == 4 {
+			if src[3] == '=' {
+				written--
+			}
+			if src[2] == '=' {
+				written--
+			}
+		}
+	}
+	return written
 }
 
 // From the go std lib - http://golang.org/src/encoding/base64/base64.go
@@ -131,10 +180,10 @@ func Encode(dst, src []byte) {
 		}
 
 		// Encode 6-bit blocks using the base64 alphabet
-		dst[0] = base64vals[b0]
-		dst[1] = base64vals[b1]
-		dst[2] = base64vals[b2]
-		dst[3] = base64vals[b3]
+		dst[0] = base64enc[b0]
+		dst[1] = base64enc[b1]
+		dst[2] = base64enc[b2]
+		dst[3] = base64enc[b3]
 
 		// Pad the final quantum
 		if len(src) < 3 {
