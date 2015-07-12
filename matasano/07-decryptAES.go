aebc43a2d2817f3eacd526b032f763b38c240ade
Krishna Sundarram <krishna.sundarram@gmail.com>
2015-07-12 18:48:17 +0530

    Add decrypt()

--- matasano/07-decryptAES.go
+++ matasano/07-decryptAES.go
@@ -2,6 +2,7 @@ package matasano
 
 import (
 	"encoding/base64"
+	"fmt"
 	"io/ioutil"
 )
 
@@ -10,19 +11,58 @@ func DecryptAES(filepath string, key []byte) string {
 	encoded, _ := ioutil.ReadFile(filepath)
 	b := make([]byte, (len(encoded)/4)*3)
 	base64.StdEncoding.Decode(b, encoded)
-	return ""
+
+	state := make([]uint32, len(b)/4)
+	for i := range state {
+		state[i] = uint32(b[i*4])<<24 | uint32(b[i*4+1])<<16 | uint32(b[i*4+2])<<8 | uint32(b[i*4+3])
+	}
+
+	expkey := keyExpansion(key)
+	nwords := len(key) / 4
+	for i := 0; i < len(state); {
+		decrypt(state[i:i+nwords], expkey)
+		i += 4
+	}
+
+	for i := range state {
+		b[i*4] = byte((state[i] >> 24) & 0xff)
+		b[i*4+1] = byte((state[i] >> 16) & 0xff)
+		b[i*4+2] = byte((state[i] >> 8) & 0xff)
+		b[i*4+3] = byte((state[i]) & 0xff)
+	}
+	fmt.Println(string(b[0:160]))
+
+	return string(b)
+}
+
+func decrypt(state, expkey []uint32) {
+	nwords := len(state)
+	keyi := len(expkey) - nwords
+	addRoundKey(state, expkey[keyi:keyi+nwords])
+	keyi -= nwords
+	rounds := len(expkey)/4 - 2
+	for i := 0; i < rounds; i++ {
+		invShiftRows(state)
+		invSubBytes(state)
+		addRoundKey(state, expkey[keyi:keyi+nwords])
+		keyi -= 4
+		invMixColumns(state)
+	}
+	invShiftRows(state)
+	invSubBytes(state)
+	addRoundKey(state, expkey[keyi:keyi+nwords])
 }
 
 func invShiftRows(state []uint32) {
-	rotWordLeft(state[1], 1)
-	rotWordLeft(state[2], 2)
-	rotWordLeft(state[3], 3)
+	for i := 1; i < len(state); i++ {
+		state[i] = rotWordRight(state[i], uint(i))
+	}
 }
 
 func shiftRows(state []uint32) {
-	rotWordRight(state[1], 1)
-	rotWordRight(state[2], 2)
-	rotWordRight(state[3], 3)
+	for i := 1; i < len(state); i++ {
+		state[i] = rotWordLeft(state[i], uint(i))
+	}
 }
 
 func invSubBytes(state []uint32) {
@@ -43,52 +83,51 @@ func addRoundKey(state, key []uint32) {
 	}
 }
 
-// based on https://en.wikipedia.org/wiki/Rijndael_mix_columns
+// based on https://en.wikipedia.org/wiki/Rijndael_mix_columns#InverseMixColumns
 func invMixColumns(state []uint32) {
-	var i uint
-	for ; i < 4; i++ {
-		var a0, a1, a2, a3 byte
-		a0 = byte((state[0] >> ((3 - i) * 8)) & 0xff)
-		a1 = byte((state[1] >> ((3 - i) * 8)) & 0xff)
-		a2 = byte((state[2] >> ((3 - i) * 8)) & 0xff)
-		a3 = byte((state[3] >> ((3 - i) * 8)) & 0xff)
-
-		var r0, r1, r2, r3 byte
+	// a0-3 represent the bytes of a column
+	// r0-3 are the transformed bytes
+	calcInvMixCols := func(a0, a1, a2, a3 byte) (r0, r1, r2, r3 byte) {
 		r0 = gMulBy14[a0] ^ gMulBy11[a1] ^ gMulBy13[a2] ^ gMulBy9[a3]
 		r1 = gMulBy9[a0] ^ gMulBy14[a1] ^ gMulBy11[a2] ^ gMulBy13[a3]
 		r2 = gMulBy13[a0] ^ gMulBy9[a1] ^ gMulBy14[a2] ^ gMulBy11[a3]
 		r3 = gMulBy11[a0] ^ gMulBy13[a1] ^ gMulBy9[a2] ^ gMulBy14[a3]
-
-		var mask uint32
-		mask = 0xff << ((3 - i) * 8)
-		mask = ^mask // used to clear those bits
-		state[0] = (state[0] & mask) | (uint32(r0) << ((3 - i) * 8))
-		state[1] = (state[1] & mask) | (uint32(r1) << ((3 - i) * 8))
-		state[2] = (state[2] & mask) | (uint32(r2) << ((3 - i) * 8))
-		state[3] = (state[3] & mask) | (uint32(r3) << ((3 - i) * 8))
+		return
 	}
+	manipulateColumns(state, calcInvMixCols)
 }
 
-// based on https://en.wikipedia.org/wiki/Rijndael_mix_columns
+// based on https://en.wikipedia.org/wiki/Rijndael_mix_columns#MixColumns
 func mixColumns(state []uint32) {
+	// a0-3 represent the bytes of a column
+	// r0-3 are the transformed bytes
+	calcMixCols := func(a0, a1, a2, a3 byte) (r0, r1, r2, r3 byte) {
+		r0 = gMulBy2[a0] ^ gMulBy3[a1] ^ a2 ^ a3
+		r1 = a0 ^ gMulBy2[a1] ^ gMulBy3[a2] ^ a3
+		r2 = a0 ^ a1 ^ gMulBy2[a2] ^ gMulBy3[a3]
+		r3 = gMulBy3[a0] ^ a1 ^ a2 ^ gMulBy2[a3]
+		return
+	}
+	manipulateColumns(state, calcMixCols)
+}
+
+func manipulateColumns(state []uint32, calc func(byte, byte, byte, byte) (byte, byte, byte, byte)) {
 	var i uint
 	for ; i < 4; i++ {
+		// Read column by column
 		var a0, a1, a2, a3 byte
 		a0 = byte((state[0] >> ((3 - i) * 8)) & 0xff)
 		a1 = byte((state[1] >> ((3 - i) * 8)) & 0xff)
 		a2 = byte((state[2] >> ((3 - i) * 8)) & 0xff)
 		a3 = byte((state[3] >> ((3 - i) * 8)) & 0xff)
 
-		var r0, r1, r2, r3 byte
-		r0 = gMulBy2[a0] ^ gMulBy3[a1] ^ a2 ^ a3
-		r1 = a0 ^ gMulBy2[a1] ^ gMulBy3[a2] ^ a3
-		r2 = a0 ^ a1 ^ gMulBy2[a2] ^ gMulBy3[a3]
-		r3 = gMulBy3[a0] ^ a1 ^ a2 ^ gMulBy2[a3]
+		// calculate the transformed bytes
+		r0, r1, r2, r3 := calc(a0, a1, a2, a3)
 
-		var mask uint32
+		var mask uint32 // used to clear those bits
 		mask = 0xff << ((3 - i) * 8)
-		mask = ^mask // used to clear those bits
-		// fmt.Println(state[0])
+		mask = ^mask
+		// set the column with the calculated values
 		state[0] = (state[0] & mask) | (uint32(r0) << ((3 - i) * 8))
 		state[1] = (state[1] & mask) | (uint32(r1) << ((3 - i) * 8))
 		state[2] = (state[2] & mask) | (uint32(r2) << ((3 - i) * 8))
@@ -117,8 +156,6 @@ func keyExpansion(key []byte) []uint32 {
 	}
 
 	for i < nwords*(rounds+1) {
-		// equivalent to
-		// expkeys[i] = (subWord(rotWord(expkeys[i-1])) ^ rcon(1/nwords)) ^ expkeys[i-nwords]
 		expkeys[i] = expkeys[i-1]
 		expkeys[i] = rotWordLeft(expkeys[i], 1)
 		expkeys[i] = subWord(expkeys[i])
