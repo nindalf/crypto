277c2f16a2de5d89af7c276e8cc6390843b97e77
Krishna Sundarram <krishna.sundarram@gmail.com>
2015-08-11 23:34:47 +0530

    Finish Set 2, Challenge 12

--- matasano/12-break_aes_ecb.go
+++ matasano/12-break_aes_ecb.go
@@ -1,67 +1,13 @@
 package matasano
 
-import (
-	"bytes"
-	"fmt"
+import "bytes"
+
+var (
+	bsize = 16
+	key   = randbytes(16)
 )
 
-func breakECB() {
-	blocksize := 16
-	chosens := genChosenCiphers(blocksize)
-	var decrypted bytes.Buffer
-	previous := make([]byte, blocksize-1)
-	for i := 0; i < unknownTextLen(); i += blocksize {
-		c := make([][]byte, 0, blocksize-1)
-		for j := 0; j < blocksize-1; j++ {
-			c = append(c, chosens[j][i:i+blocksize])
-		}
-		previous = decrypt16bytes(c, previous)
-		decrypted.Write(previous)
-	}
-	fmt.Println(decrypted.String())
-}
-
-func decrypt16bytes(chosens [][]byte, previous []byte) []byte {
-	decrypted := make([]byte, 0, 16)
-	for i := 14; i >= 0; i-- {
-		previous = previous[1:len(previous)]
-		dec := decryptbyte(chosens[i], previous)
-		previous = append(previous, dec)
-		decrypted = append(decrypted, dec)
-	}
-	return decrypted
-}
-
-func decryptbyte(chosen, previous []byte) byte {
-	c := string(chosen)
-	for i := 0; i < 255; i++ {
-		p := append(previous, byte(i))
-		if string(oracle(p)[0:16]) == c {
-			return byte(i)
-		}
-	}
-	return 0
-}
-
-func genChosenCiphers(blocksize int) [][]byte {
-	chosens := make([][]byte, 0, blocksize-1)
-	prefix := make([]byte, 1, blocksize-1)
-	var x byte
-	for i := 0; i < blocksize-1; i++ {
-		chosens = append(chosens, oracle(prefix))
-		prefix = append(prefix, x)
-	}
-	return chosens
-}
-
-func unknownTextLen() int {
-	var b []byte
-	return len(oracle(b))
-}
-
 //  a function that produces: AES-128-ECB(b || unknown-string, random-key)
-var key = randbytes(16)
-
 func oracle(b []byte) []byte {
 	plaintext := []byte("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
 	dec := make([]byte, (3*len(plaintext))/4)
@@ -70,4 +16,52 @@ func oracle(b []byte) []byte {
 	b = padPKCS7(b, 16)
 	EncryptAESECB(b, key)
 	return b
+}
+
+// BreakECB decrypts a ciphertext received from the oracle function (defined above)
+// It does so by repeated calls to the oracle
+// This solves http://cryptopals.com/sets/2/challenges/12/
+func BreakECB() []byte {
+	chosens := genChosenCiphers()
+	var decrypted bytes.Buffer
+	previous := make([]byte, bsize, len(chosens[0]))
+	for i := 0; i < len(chosens[0]); i += bsize {
+		previous = decrypt16bytes(chosens, previous, i)
+		decrypted.Write(previous)
+	}
+	return decrypted.Bytes()
+}
+
+func decrypt16bytes(chosens [][]byte, previous []byte, index int) []byte {
+	decrypted := make([]byte, 0, bsize)
+	for i := len(chosens) - 1; i >= 0; i-- {
+		previous = previous[1:len(previous)]
+		dec := decryptbyte(chosens[i][index:index+bsize], previous)
+		previous = append(previous, dec)
+		decrypted = append(decrypted, dec)
+	}
+	return decrypted
+}
+
+func decryptbyte(chosen, previous []byte) byte {
+	previous = append(previous, byte(0))
+	for i := 0; i < 255; i++ {
+		previous[bsize-1] = byte(i)
+		if bytes.Equal(oracle(previous)[0:bsize], chosen) {
+			return byte(i)
+		}
+	}
+	return 0
+}
+
+func genChosenCiphers() [][]byte {
+	chosens := make([][]byte, 0, bsize)
+	prefix := make([]byte, 0, bsize-1)
+	chosens = append(chosens, oracle(prefix))
+	var x byte
+	for i := 0; i < bsize-1; i++ {
+		prefix = append(prefix, x)
+		chosens = append(chosens, oracle(prefix))
+	}
+	return chosens
 }
